02ebffc0a0661837ee2143cf99c87fc9630a9e9a
TBEverett <tbarroseverett@gmail.com>
2023-10-11 15:18:36 -0300

    updated gitignore

--- ALL.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ncores=2
-
-seedList="0 1"
-
-parallel -j ${ncores} bash execute.sh {} ::: ${seedList}
-
-
--- execute.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-seed=$1
-baseDir="baseTuning"
-newDir=S${seed}_${baseDir}
-cp -r ${baseDir} ${newDir}
-cd ${newDir}
-nohup bash ToDoParamILS.sh toTune/SolveVRP.tune ${seed} > OUT &
-
